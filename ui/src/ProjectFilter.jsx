d6a27d72edc91615624bec726d5c7a8b8fdbecbd
Jon Lane <admiralorbiter@gmail.com>
2021-12-31 23:11:41 -0600

    Filters

--- ui/src/ProjectFilter.jsx
+++ ui/src/ProjectFilter.jsx
@@ -1,37 +1,69 @@
 /* eslint "react/prefer-stateless-function": "off" */
 
 import React from 'react';
+import URLSearchParams from 'url-search-params';
 import {Link, withRouter} from 'react-router-dom';
 
 
 
 class ProjectFilter extends React.Component {
-  constructor() {
+  constructor({location: {search}}) {
     super();
+    const params = new URLSearchParams(location.search);
+    this.state={status: params.get('status')|| '', changed: false};
+
     this.onChangeStatus = this.onChangeStatus.bind(this);
+    this.applyFilter = this.applyFilter.bind(this);
+    this.showOriginalFilter = this.showOriginalFilter.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { location: {search: prevSearch} } = prevProps;
+    const { location: {search} } = this.props;
+    if(prevSearch !== search){this.showOriginalFilter();}
   }
 
   onChangeStatus(e){
-    const status = e.target.value;
-    const{history} = this.props;
+    this.setState({status: e.target.value, changed: true});
+  }
+
+  applyFilter(){
+    const {status} = this.state;
+    const {history} = this.props;
     history.push({
       pathname: '/projects',
-      search: status ? `?status=${status}` : '',
+      search: status? `?status=${status}`: '',
+    })
+  }
+
+  showOriginalFilter() {
+    const { location: { search } } = this.props;
+    const params = new URLSearchParams(search);
+    this.setState({
+    status: params.get('status') || '',
+    changed: false,
     });
   }
 
   render() {
+    const{status, changed} = this.state;
+    const {location: {search}} = this.props;
+    const params = new URLSearchParams(search);
     return (
       <div>
         Status:
         {' '}
-        <select onChange={this.onChangeStatus}>
+        <select value={status} onChange={this.onChangeStatus}>
           <option value="">(All)</option>
           <option value="New">New</option>
           <option value="Assigned">Assigned</option>
           <option value="Fixed">Fixed</option>
           <option value="Closed">Closed</option>
         </select>
+        {' '}
+        <button type = "button" onClick = {this.applyFilter}>Apply</button>
+        {' '}
+        <button type = "button" onClick = {this.showOriginalFilter} disabled={!changed}>Reset</button>
       </div>
     );
   }
