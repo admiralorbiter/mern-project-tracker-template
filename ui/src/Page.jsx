38414f05a0b417fc1966a6e2b17298eb6069a7fd
Jon Lane <admiralorbiter@gmail.com>
2022-01-07 11:35:25 -0600

    added a nav bar

--- ui/src/Page.jsx
+++ ui/src/Page.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 import {NavLink} from 'react-router-dom'
+import {Navbar, Nav, NavItem, OverlayTrigger, Tooltip, Dropdown} from 'react-bootstrap'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPlusCircle} from '@fortawesome/free-solid-svg-icons';
 
 import Contents from "./Contents.jsx";
 
 function NavBar(){
     return (
-        <nav>
-            <NavLink exact to="/">Home</NavLink>
-            {' | '}
-            <NavLink to="/projects">Project List</NavLink>
-            {' | '}
-            <NavLink to="/report">Report</NavLink>
-        </nav>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand>
+                Project Tracker
+            </Navbar.Brand>
+            <Nav>
+                <Nav.Link href="/">Home</Nav.Link>
+                {' | '}
+                <Nav.Link href="/projects">Project List</Nav.Link>
+                {/*NavLink works smoother, but not sure how to style like the bootstrap buttons yet*/}
+                {/* <NavLink to="/projects">Project List</NavLink> */}
+                {' | '}
+                <Nav.Link href="/report">Report</Nav.Link>
+                {/* <NavLink to="/report">Report</NavLink> */}
+            </Nav>
+            <Nav pullRight>
+                {/* <NavItem>
+                    <OverlayTrigger
+                        placement="left"
+                        delayShow={1000}
+                        overlay={<Tooltip id="create-project">Create Project</Tooltip>}
+                    >
+                         <FontAwesomeIcon icon={faPlusCircle}/>
+                    </OverlayTrigger>
+                </NavItem> */}
+                <Dropdown>
+                    <Dropdown.Item href="#/action-1">About</Dropdown.Item>
+                </Dropdown>
+            </Nav>
+        </Navbar>
     )
 }
 
